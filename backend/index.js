7121175cefcc22e392c3093e60c37d5c2012b454
yungflashh <streaktv8@gmail.com>
2025-01-01 16:53:16 +0100

    changed the cors origin options

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 const corsOptions = {
-    origin: '*', 
+    origin: 'http://localhost:5174',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, 
