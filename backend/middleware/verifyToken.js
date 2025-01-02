b3d3c3c72df018793fab0e6eb864966cf5f1ea92
yungflashh <streaktv8@gmail.com>
2025-01-02 08:28:48 +0100

    Now using token to verify

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,30 +1,6 @@
 import jwt from "jsonwebtoken"
 
 
-const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;  // Assuming the token is stored in cookies
-
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "No token provided. Unauthorized access."
-        });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);  // Decode the token using the secret
-        req.userId = decoded.userId;  // Attach the userId to the request object for later use
-        next();
-    } catch (error) {
-        return res.status(403).json({
-            success: false,
-            message: "Invalid token. Unauthorized access."
-        });
-    }
-};
-
-
-
 export const verifytoken = (req, res, next)=>{
     const token = req.cookies.token
     if(!token) return res.status(401).json({success:false, message:"Unauthorized - no token provided"})
