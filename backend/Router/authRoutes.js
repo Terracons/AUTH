a02598bf2b20433a7587148bd7d55de8adef6e33
yungflashh <streaktv8@gmail.com>
2025-01-03 19:34:16 +0100

    testing the payment gateway

--- backend/Router/authRoutes.js
+++ backend/Router/authRoutes.js
@@ -52,7 +52,7 @@ import { authenticateToken, verifytoken } from "../middleware/verifyToken.js"
  router.get('/notifications',authenticateToken, verifytoken,getNotifications);
  router.post('/analytics/:promiseId', analytics);  
  router.get("/getUsername", verifytoken, getUsername)
- router.post ("/paystack/payment", paymentGateway)
+ router.post ("/paystack/payment", verifytoken, paymentGateway)
  router.post('/payment/verify', verifytoken, paymentVerification)
  router.get('/get-user-email', getEmail)
 
